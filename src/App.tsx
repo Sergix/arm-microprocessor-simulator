a76898b64baa9a8e81a87692722774cb2084fbcb
sergix <me@sergix.dev>
2022-09-03 17:51:48 -0400

    update: frontend state loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,95 @@
 import { Component, Show } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect, onMount, onCleanup } from 'solid-js';
 import { invoke } from '@tauri-apps/api/tauri'
+import { listen } from '@tauri-apps/api/event'
 import { open } from '@tauri-apps/api/dialog'
 import * as log from 'tauri-plugin-log-api'
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 import MemoryGrid from './MemoryGrid';
 import { style } from 'solid-js/web/types';
 
+const MEMORY_ROW_SIZE = 16;
+
+// from backend memory.rs
+interface IMemoryPayload {
+	checksum: number
+	loaded: boolean
+	memory_array: Array<number>
+	error: string
+	filename: string
+}
+
 const App: Component = () => {
-  log.attachConsole();
+	log.attachConsole();
+	
+	const [memory, setMemory] = createSignal(new Array<Array<number>>())
+	const [checksum, setChecksum] = createSignal(0)
+	const [filename, setFilename] = createSignal("")
+	
+	// split into row chunks
+    const chunk = (payload_memory_array: Array<number>) => {
+        let memory_array = new Array<Array<number>>();
+        while (payload_memory_array.length > 0)
+            memory_array.push(payload_memory_array.splice(0, MEMORY_ROW_SIZE))
 
-  const [filename, setFilename] = createSignal("")
+        return memory_array
+    }
 
-  const handleLoad = async () => {
-    const selected = await open({
-      title: "Select ELF binary"
-    })
-    setFilename(() => (selected?.toString() || ""))
-    
-    const res: string = await invoke('cmd_load_elf', { filename: selected });
-    log.trace("SolidJS[App]: Called loader");
-  };
-
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <h1 class="logo">ARMSim</h1>
-      </header>
-      <p class={styles.filename}>{ filename() === "" ? "None." : filename }</p>
-      <button class={styles.file_loader_button} onClick={handleLoad}>
-        Load ELF
-      </button>
-      <Show when={filename() !== ""}>
-        <MemoryGrid />
-      </Show>
-    </div>
-  );
-};
-
-export default App;
+	const setMemoryFromPayload = (payload: IMemoryPayload) => setMemory(chunk(payload.memory_array))
+	
+	createEffect(() => {
+		listen('elf_load', ({ payload }: { payload: IMemoryPayload }) => {
+			log.trace("SolidJS[App]: loading ELF...")
+			setChecksum(payload.checksum)
+			setMemoryFromPayload(payload)
+		});
+		
+		// TODO: get unlistener for unmount (if needed)
+		// https://github.com/FabianLars/mw-toolbox/blob/main/gui/src/pages/Upload/Upload.tsx#L70
+	})
+	
+	// check if a binary has been loaded by command-line args
+	onMount(() => {
+		invoke('cmd_get_memory')
+		.then((payload: any) => {
+			log.trace("SolidJS[App.onMount]: loaded elf")
+			setMemoryFromPayload(payload)
+		})
+		.catch((message: any) => {
+			log.trace(`SolidJS[App.onMount]: no elf loaded`)
+		})
+	})
+	
+	onCleanup(() => {
+		// TODO: call unlistener for elf_load (see above)
+	})
+	
+	const handleLoad = async () => {
+		const selected = await open({
+			title: "Select ELF binary"
+		})
+		setFilename(() => (selected?.toString() || ""))
+		
+		const res: string = await invoke('cmd_load_elf', { filename: selected });
+		log.trace("SolidJS[App.handleLoad]: Called loader");
+	};
+	
+	return (
+		<div class={styles.App}>
+			<header class={styles.header}>
+				<h1 class="logo">ARMSim</h1>
+			</header>
+			<p class={styles.filename}>{ filename() === "" ? "None." : filename }</p>
+			<button class={styles.file_loader_button} onClick={handleLoad}>
+				Load ELF
+			</button>
+			<Show when={checksum() != 0}>
+				<MemoryGrid checksum={checksum()} memory={memory()}/>
+			</Show>
+		</div>
+		);
+	};
+	
+	export default App;
+	
--- src/MemoryGrid.tsx
+++ src/MemoryGrid.tsx
@@ -1,66 +1,27 @@
-import { Component, onMount, createSignal, onCleanup, createEffect, indexArray, Show } from "solid-js"
+import { Component, createSignal, onMount, onCleanup, createEffect, indexArray, Show, mapArray } from "solid-js"
 import { listen } from '@tauri-apps/api/event'
-import { invoke } from "@tauri-apps/api/tauri"
+import { invoke } from '@tauri-apps/api/tauri'
 import * as log from 'tauri-plugin-log-api'
 
 import styles from './MemoryGrid.module.css'
 
-
-const MEMORY_ROW_SIZE = 16;
-
-// from backend memory.rs
-interface IMemoryPayload {
+interface IMemoryGridProps {
+    memory: Array<Array<number>>
     checksum: number
-    loaded: boolean
-    memory_array: Array<number>
-    error: string
 }
 
-const MemoryGrid: Component = () => {
-    const [filename, setFilename] = createSignal("")
-    const [memory, setMemory] = createSignal(new Array<Array<number>>())
-    const [checksum, setChecksum] = createSignal(0)
-
-    createEffect(() => {
-        listen('elf_load', ({ payload }: { payload: IMemoryPayload }) => {
-            // split into row chunks
-            let memory_array = new Array<Array<number>>();
-            while (payload.memory_array.length > 0)
-                memory_array.push(payload.memory_array.splice(0, MEMORY_ROW_SIZE))
-
-            setChecksum(payload.checksum)
-            setMemory(memory_array)
-        });
-
-        // TODO: get unlistener for unmount (if needed)
-        // https://github.com/FabianLars/mw-toolbox/blob/main/gui/src/pages/Upload/Upload.tsx#L70
-    });
-    
-    onMount(() => {
-        invoke('cmd_get_memory').then((message) => {
-            console.log(message)
-        })
-        .catch((message) => {
-            console.log("ERROR: not yet loaded")
-        })
-    })
-
-    onCleanup(() => {
-        // TODO: call unlistener for elf_load (see above)
-    })
-
+const MemoryGrid: Component<IMemoryGridProps> = (props: IMemoryGridProps) => {
     return (
-        <Show when={memory().length > 0}>
-            {/* <span>Checksum: {checksum}</span> */}
+        <Show when={props.memory.length > 0}>
             <table class={styles.MemoryGrid} onScroll={() => {}}>
-                <caption>Checksum: <span style="font-family: monospace;">{checksum}</span></caption>
+                <caption>Checksum: <span style="font-family: monospace;">{props.checksum}</span></caption>
                 
                 {/* https://stackoverflow.com/questions/70819075/solidjs-for-vs-index */}
-                {indexArray(memory, (row, index) => {
+                {props.memory.map((row, index) => {
                     return (
                         <tr>
                             <td>{(index * 16).toString(16).padStart(8, '0')}</td>
-                            {row().map((item, i) => {
+                            {row.map((item, i) => {
                                 return (
                                     <td>{item.toString(16).padStart(2, '0')}</td>
                                 )
